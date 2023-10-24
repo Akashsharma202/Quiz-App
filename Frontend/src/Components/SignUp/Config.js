85fb26071a9beaa2278adca3e06fdaeaf9fad15d
Akash Sharma <akash@pop-os.localdomain>
2023-10-24 23:15:05 +0530

    added route url in frontend

--- Frontend/src/Components/SignUp/Config.js
+++ Frontend/src/Components/SignUp/Config.js
@@ -13,13 +13,14 @@ import {
 // const dapp=express();
 // dapp.use(cors());
 const firebaseConfig = {
-  apiKey: "AIzaSyCMcbi0YaVE9DT8Cn0MqbmwZMgJhsDv3xo",
-  authDomain: "quizapp-d8025.firebaseapp.com",
-  projectId: "quizapp-d8025",
-  storageBucket: "quizapp-d8025.appspot.com",
-  messagingSenderId: "895896033608",
-  appId: "1:895896033608:web:bf83905cf6369c13417b7d",
-  measurementId: "G-WF0NGN2FQV"
+  apiKey: "AIzaSyB8n55Gdq35qYnceVQA5i2nUKYGaPYrrnw",
+  authDomain:"njwbfjd.firebaseapp.com",
+  databaseURL: "https://njwbfjd-default-rtdb.firebaseio.com",
+  projectId: "njwbfjd",
+  storageBucket: "njwbfjd.appspot.com",
+  messagingSenderId: "759024699998",
+  appId: "1:759024699998:web:afd249dc7606fd9e2b02cf",
+  measurementId: "G-2PSFKEGRMS"
 };
 const app = initializeApp(firebaseConfig);
 export const auth =  getAuth(app);
